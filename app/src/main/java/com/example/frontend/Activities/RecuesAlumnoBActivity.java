8c0842620750f7267fde0bc1ec020dcb17eb3654
bryanpalacios20 <80603906+bryanpalacios20@users.noreply.github.com>
2022-01-22 00:45:25 -0500

    Interfaz Login y Registrar

---
+++ app/src/main/java/com/example/frontend/Activities/RecuesAlumnoBActivity.java
@@ -0,0 +1,14 @@
+package com.example.frontend.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecuesAlumnoBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recues_alumno_bactivity);
+    }
+}
