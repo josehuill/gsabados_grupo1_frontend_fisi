b63bb3ba39c0c6087522ea339078f438d8b3198d
bryanpalacios20 <80603906+bryanpalacios20@users.noreply.github.com>
2022-01-22 00:50:43 -0500

    Interfaz crear evento

---
+++ app/src/main/java/com/example/frontend/Activities/Map_AlumnoAActivity.java
@@ -0,0 +1,14 @@
+package com.example.frontend.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Map_AlumnoAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_alumno_aactivity);
+    }
+}
